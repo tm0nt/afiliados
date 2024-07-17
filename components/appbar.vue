56856fe503118d179ff6eeb0ca6179aa11b98dba
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-07-17 19:18:34 -0300

    update

--- components/appbar.vue
+++ components/appbar.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app-bar color="background">
     <v-app-bar-title>
-          <v-img class="mx-auto" src="https://i.imgur.com/G9l1yU5.png" width="60"></v-img>
+          <v-img class="mx-auto" src="https://i.imgur.com/G9l1yU5.png" max-width="60"></v-img>
     </v-app-bar-title>
   </v-app-bar>
 </template>
